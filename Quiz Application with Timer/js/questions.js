ac99006d2e463b7cac58ba90b72e974509048f50
Alketaa <98833484+Alketaa@users.noreply.github.com>
2023-06-11 13:41:44 +0200

    Add files via upload

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,94 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which HTML attribute is used to define inline styles?",
+    answer: "style",
+    options: [
+      "font",
+      "style",
+      "class",
+      "styles"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How do you create a function in JavaScript?",
+    answer: "function = myFunction()",
+    options: [
+      "function = myFunction()",
+      "function:myFunction()",
+      "function myFunction()",
+      "function = myFunction"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How to write an IF statement in JavaScript",
+    answer: "if (i == 5)",
+    options: [
+      "if i = 5;",
+      "if i == 5 then;",
+      "if i = 5 then",
+      "if (i == 5)"
+    ]
+  },
+  {
+    numb: 6,
+    question: "In HTML, which attribute is used to specify that an input field must be filled out?",
+    answer: "required",
+    options: [
+      "required",
+      "validate",
+      "placeholder",
+      "formvalidate"
+    ]
+  },
+  {
+    numb: 7,
+    question: "In HTML, which attribute is used to specify that an input field must be filled out?",
+    answer: "required",
+    options: [
+      "required",
+      "validate",
+      "placeholder",
+      "formvalidate"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
